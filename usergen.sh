eac331d1d7c437df8d1311f386e1d51bc5adebf1
Kaushik VR <vrkaushik20@gmail.com>
2023-10-30 12:04:57 -0400

    Create users
    This script will create users and their home directories on the machine. This will also generate ed25519 ssh key pairs for each users. These user will also be added to the sudoers file.

---
+++ usergen.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# List of user names
+USERS=("kaushik" "chris" "haneesha" "april" "yiduo")
+
+# Output file to store public keys
+OUTPUT_FILE="public_keys.txt"
+
+# Loop through the list and create users, home directories, and Ed25519 SSH key pairs
+for USER in "${USERS[@]}"
+do
+    # Create user if not already exists
+    if id "$USER" &>/dev/null; then
+        echo "User $USER already exists. Skipping."
+    else
+        sudo useradd -m -s /bin/bash "$USER"
+        echo "User $USER created successfully."
+    fi
+
+    # Create SSH directory and generate Ed25519 key pair
+    SSH_DIR="/home/$USER/.ssh"
+    if [ ! -d "$SSH_DIR" ]; then
+        sudo -u "$USER" mkdir "$SSH_DIR"
+        sudo -u "$USER" ssh-keygen -t ed25519 -f "$SSH_DIR/id_ed25519" -N ""
+        echo "Ed25519 SSH key pair generated for $USER."
+        
+        # Append public key to the output file
+        cat "$SSH_DIR/id_ed25519.pub" >> "$OUTPUT_FILE"
+        echo "Public key for $USER added to $OUTPUT_FILE."
+    else
+        echo "SSH directory already exists for $USER. Skipping key pair generation."
+    fi
+
+     # Add user to sudoers file
+    if sudo grep -q "$USER" /etc/sudoers; then
+        echo "User $USER is already in sudoers file. Skipping."
+    else
+        echo "$USER ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
+        echo "User $USER added to sudoers file."
+    fi
+    
+done
+
+echo "User creation and Ed25519 SSH key generation completed."
