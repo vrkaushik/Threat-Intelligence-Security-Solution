5caa8e0e53c5ad4403121d041cff3c6b6c701212
Kaushik VR <vrkaushik@Kaushiks-MacBook-Pro.local>
2023-11-07 16:06:05 -0500

    Adding the variables for tf config

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "Google Cloud Project ID"
+}
+
+variable "region" {
+  description = "GCP region for resources"
+  default     = "us-east1"
+}
+
+variable "zone" {
+  description = "GCP zone for instances"
+  default     = "us-east1-b"
+}
+
