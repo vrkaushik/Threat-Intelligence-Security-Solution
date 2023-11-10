40de0f156e15fa9319caa26c636a31dba77f1105
cto999 <57422434+cto999@users.noreply.github.com>
2023-11-10 18:54:48 -0500

    feat: cleanup terraform file

--- main.tf
+++ main.tf
@@ -4,21 +4,17 @@ provider "google" {
   project = var.project_id
 }
 
-######################################--------OpenCTI VPC--------######################################
+# OpenCTI
+
+# OpenCTI VPC
 resource "google_compute_network" "opencti-vpc" {
   name                    = "opencti-vpc"
   auto_create_subnetworks = true
 }
 
-# # Create a subnet within the VPC network
-# resource "google_compute_subnetwork" "subnet" {
-#   name          = "opencti-network"
-#   ip_cidr_range = "10.0.1.0/24"
-#   region        = var.region
-#   network       = google_compute_network.vpc_network.name
-# }
-
 # Firewall Rules for OpenCTI 
+
+# Allow TCP ports 1514 and 1515 from Wazuh
 resource "google_compute_firewall" "opencti-allow-wazuh" {
     name = "opencti-allow-wazuh"
     network = google_compute_network.opencti-vpc.name
@@ -27,19 +23,20 @@ resource "google_compute_firewall" "opencti-allow-wazuh" {
         protocol =  "tcp"
         ports = ["1514", "1515"]        
     }
-
-    #source_ranges = [google_compute_instance.wazuh.network_interface.0.network_ip]  
+ 
     source_tags = ["wazuh"]
     #source_ranges = [google_compute_instance.wazuh.network_interface.0.network_ip]  
     target_tags = ["opencti"] 
 }
 
+# Allow TCP port 33333 (SSH) from Bastion
 resource "google_compute_firewall" "opencti-allow-ssh" {
   name = "opencti-allow-ssh"
   network = google_compute_network.opencti-vpc.name
   
   allow {
     protocol = "tcp"
+    # ports = ["33333"]
     ports = ["22"]
   }
 
@@ -48,6 +45,7 @@ resource "google_compute_firewall" "opencti-allow-ssh" {
   target_tags = ["opencti"]
 }
 
+# Allow TCP port 8080 within OpenCTI
 resource "google_compute_firewall" "opencti-allow-http" {
   name = "opencti-allow-http"
   network = google_compute_network.opencti-vpc.name
@@ -57,11 +55,11 @@ resource "google_compute_firewall" "opencti-allow-http" {
     ports = ["8080"]
   }
 
-  #source_ranges = [google_compute_instance.bastion.network_interface.0.network_ip]  
   source_tags = ["opencti"]
   target_tags = ["opencti"]
 }
 
+# Allow ports 443 and 9000 from any source to OpenCTI
 resource "google_compute_firewall" "opencti-allow-es" {
   name = "opencti-allow-es"
   network = google_compute_network.opencti-vpc.name
@@ -75,31 +73,13 @@ resource "google_compute_firewall" "opencti-allow-es" {
   target_tags = ["opencti"]
 }
 
-
-######################################--------OpenCTI--------######################################
-
-
 # Reserve a static external IP address for OpenCTI
 resource "google_compute_address" "opencti_static_ip" {
   name   = "opencti-static-ip"
   region = var.region
 }
 
-
-# # Create a GCP instance within the specified subnet and with the reserved static IP
-# # OpenCTI instance
-# data "template_file" "startup_script" {
-#   template = file("deps.sh")
-#   # The wrapper script is used by each of the providers and each variable has to be filled out in order to run. Unfortunately, this means that if you change something in one provider, you have to change it in each of the others. It's not ideal, but FYI.
-#   vars = {
-#     "external_ip"="${google_compute_address.opencti_static_ip.address}"
-#     "docker_compose_file" = file("opencti/docker-compose.yml")
-#     "nginx_conf_file" = file("opencti/nginx/conf.d/default.conf")
-#   }
-# }
-
-
-
+# Provision OpenCTI machine
 resource "google_compute_instance" "opencti" {
   name         = "opencti"
   machine_type = "e2-highmem-4"
@@ -115,7 +95,6 @@ resource "google_compute_instance" "opencti" {
 
   network_interface {
     network = google_compute_network.opencti-vpc.self_link
-    # subnetwork = google_compute_subnetwork.subnet.self_link
     access_config {
       nat_ip = google_compute_address.opencti_static_ip.address
     }
@@ -125,22 +104,14 @@ resource "google_compute_instance" "opencti" {
     ssh-keys = "admin:${file(var.public_key)}"
   }
 
-  # metadata = {
-  #   startup-script = templatefile("deps.sh", {
-  #     //external_ip           = google_compute_address.opencti_static_ip.address,
-  #     docker_compose_file   = file("opencti/docker-compose.yml"),
-  #     nginx_conf_file       = file("opencti/nginx/conf.d/default.conf")
-  #   })
-  # }
-
   connection {
     type = "ssh"
     host = google_compute_address.opencti_static_ip.address
     user = "admin"
-    # private_key = file("id_ed25519.key")
     private_key = file(var.private_key)
   }
 
+  # Copy OpenCTI setup script
   provisioner "file" {
     destination = "/home/admin/deps.sh"
     content = templatefile("deps.sh", {
@@ -151,49 +122,38 @@ resource "google_compute_instance" "opencti" {
     })
   }
 
+  # Copy docker compose
   provisioner "file" {
     source = "opencti/docker-compose.yml"
     destination = "/home/admin/docker-compose.yml"
-    # content = templatefile("opencti/docker-compose.yml")
   }
 
+  # Copy nginx config
   provisioner "file" {
     source = "opencti/nginx/conf.d/default.conf"
     destination = "/home/admin/default.conf"
-    # content = templatefile("opencti/nginx/conf.d/default.conf")
   }
 
+  # Execute setup script
   provisioner "remote-exec" {
     inline = ["/bin/bash /home/admin/deps.sh"]
   }
 
-# metadata_startup_script = <<-EOF
-#     #!/bin/bash
-#     touch hello.txt
-#   EOF
-
   tags = ["opencti"]
 }
 
-# output "rendered_deps" {
-#   value = templatefile("deps.sh", {
-#       wazuh_external_ip     = google_compute_address.wazuh_static_ip.address,
-#       docker_compose_file   = file("opencti/docker-compose.yml"),
-#       nginx_conf_file       = file("opencti/nginx/conf.d/default.conf")
-#     })
-# }
+# Wazuh
 
+# Wazuh Firewall Rules
 
-
-
-#Firewall Rules for Wazuh
-
+# Allow TCP on port 33333 (SSH) from bastion
 resource "google_compute_firewall" "wazuh-allow-ssh" {
   name = "wazuh-allow-ssh"
   network = google_compute_network.opencti-vpc.name
   
   allow {
     protocol = "tcp"
+    # ports = ["33333"]
     ports = ["22"]
   }
 
@@ -230,7 +190,7 @@ resource "google_compute_firewall" "wazuh-allow-https" {
   target_tags = ["wazuh"]
 }
 
-######################################--------Wazuh--------######################################
+# Wazuh
 
 # Reserve a static external IP address for Wazuh
 resource "google_compute_address" "wazuh_static_ip" {
@@ -238,21 +198,22 @@ resource "google_compute_address" "wazuh_static_ip" {
   region = var.region
 }
 
-# Wazuh Instance
+# Provision Wazuh Instance
 resource "google_compute_instance" "wazuh" {
   name         = "wazuh"
   machine_type = "e2-medium"
   zone         = var.zone
   project      = var.project_id
+
   boot_disk {
     initialize_params {
       image = "ubuntu-2204-jammy-v20230919"
       size = 50
     }
   }
+
   network_interface {
     network = google_compute_network.opencti-vpc.self_link
-    # subnetwork = google_compute_subnetwork.subnet.self_link
     access_config {
       nat_ip = google_compute_address.wazuh_static_ip.address
     }
@@ -266,18 +227,13 @@ resource "google_compute_instance" "wazuh" {
     type = "ssh"
     host = google_compute_address.wazuh_static_ip.address
     user = "ubuntu"
-    # private_key = file("id_ed25519.key")
     private_key = file(var.private_key)
   }
 
+  # Copy OpenCTI setup script
   provisioner "file" {
     destination = "/home/ubuntu/wazuh-deps.sh"
     source = "wazuh-deps.sh"
-    # content = templatefile("wazuh-deps.sh", {
-    #   //external_ip           = google_compute_address.opencti_static_ip.address,
-    #   docker_compose_file   = file("opencti/docker-compose.yml"),
-    #   nginx_conf_file       = file("opencti/nginx/conf.d/default.conf")
-    # })
   }
 
   provisioner "file" {
@@ -290,12 +246,13 @@ resource "google_compute_instance" "wazuh" {
     destination = "/home/ubuntu/usergen.sh"
   }
 
+  # Copy docker compse
   provisioner "file" {
     source = "wazuh/docker-compose.yml"
     destination = "/home/ubuntu/docker-compose.yml"
   }
 
-
+  # Execute setup script
   provisioner "remote-exec" {
     inline = ["/bin/bash /home/ubuntu/wazuh-deps.sh"]
   }
@@ -303,28 +260,21 @@ resource "google_compute_instance" "wazuh" {
   provisioner "remote-exec" {
     inline = ["/bin/bash /home/ubuntu/usergen.sh"]
   }
-
-
- tags = ["wazuh"]
+  
+  tags = ["wazuh"]
 }
 
+# Bastion
 
-
-######################################--------Bastion VPC--------######################################
+# Bastion VPC
 resource "google_compute_network" "bastion-vpc" {
   name                    = "bastion-vpc"
   auto_create_subnetworks = true
 }
 
-# Create a subnet within the VPC network
-# resource "google_compute_subnetwork" "subnet" {
-#   name          = "opencti_network"
-#   ip_cidr_range = "10.0.1.0/24"
-#   region        = var.region
-#   network       = google_compute_network.vpc_network.name
-# }
+# Bastion Firewall Rules
 
-# Firewall Rules
+# Allow TCP ports 1514 and 1515 from Wazuh
 resource "google_compute_firewall" "bastion-allow-wazuh" {
     name = "bastion-allow-wazuh"
     network = google_compute_network.bastion-vpc.name
@@ -338,6 +288,7 @@ resource "google_compute_firewall" "bastion-allow-wazuh" {
     target_tags = ["bastion"]
 }
 
+# Allow TCP port 33333 (SSH) from any
 resource "google_compute_firewall" "bastion-allow-ssh" {
   name = "bastion-allow-ssh"
   network = google_compute_network.bastion-vpc.name
@@ -351,46 +302,35 @@ resource "google_compute_firewall" "bastion-allow-ssh" {
   target_tags = ["bastion"]
 }
 
-######################################--------Bastion--------######################################
-
 # Reserve a static external IP address for Bastion Host
 resource "google_compute_address" "bastion_static_ip" {
   name   = "bastion-static-ip"
   region = var.region
 }
 
+# Provision Bastion machine
 resource "google_compute_instance" "bastion" {
-    name = "bastion"
-    machine_type = "e2-medium"
-    zone = var.zone
-    
-    boot_disk {
-      initialize_params {
-        image = "debian-11-bullseye-v20231010"
-        size = 10
-      }
+  name = "bastion"
+  machine_type = "e2-medium"
+  zone = var.zone
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20231010"
+      size = 10
     }
-    network_interface {
+  }
+
+  network_interface {
     network = google_compute_network.bastion-vpc.self_link
-    # subnetwork = google_compute_subnetwork.subnet.self_link
     access_config {
       nat_ip = google_compute_address.bastion_static_ip.address
     }
   }
-  
-
 
   metadata = {
     ssh-keys = "admin:${file(var.public_key)}"
   }
-
-  # metadata = {
-  #   startup-script = templatefile("deps.sh", {
-  #     //external_ip           = google_compute_address.opencti_static_ip.address,
-  #     docker_compose_file   = file("opencti/docker-compose.yml"),
-  #     nginx_conf_file       = file("opencti/nginx/conf.d/default.conf")
-  #   })
-  # }
 
   connection {
     type = "ssh"
@@ -400,18 +340,18 @@ resource "google_compute_instance" "bastion" {
     private_key = file(var.private_key)
   }
 
+  # Copy setup script
   provisioner "file" {
     destination = "/home/admin/bastion-deps.sh"
     content = templatefile("bastion-deps.sh", {
-      wazuh_external_ip     = google_compute_address.wazuh_static_ip.address,
-      #bastion_internal_ip = google_compute_instance.bastion.network_interface.0.network_ip, 
+      wazuh_external_ip     = google_compute_address.wazuh_static_ip.address
      })
   }
 
+  # Execute setup script
   provisioner "remote-exec" {
     inline = ["/bin/bash /home/admin/bastion-deps.sh"]
   }
-
-
-    tags = ["bastion"]
+  
+  tags = ["bastion"]
 }
